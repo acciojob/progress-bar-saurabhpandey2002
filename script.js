8a5b8ecde42630b37a0541d56bc20ca282c731e3
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-06-01 15:47:38 +0530

    ADD

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
 //your JS code here. If required.
 const circles = document.querySelectorAll('.circle');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
 let currentActive = 1;
 
-nextButton.addEventListener('click', () => {
-  currentActive++;
-
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
-
-  update();
+next.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
 });
 
-prevButton.addEventListener('click', () => {
-  currentActive--;
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
+prev.addEventListener('click', () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
 });
 
 function update() {
-  circles.forEach((circle, index) => {
-    if (index < currentActive) {
-      circle.classList.add('active');
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
     } else {
-      circle.classList.remove('active');
+        prev.disabled = false;
+        next.disabled = false;
     }
-  });
-
-  if (currentActive === 1) {
-    prevButton.disabled = true;
-  } else if (currentActive === circles.length) {
-    nextButton.disabled = true;
-  } else {
-    prevButton.disabled = false;
-    nextButton.disabled = false;
-  }
 }
-
